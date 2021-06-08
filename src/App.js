504556faf4f255accd2311f4ea23bfe9dfc59aa5
eullerpeixoto <eullerz01@hotmail.com>
2021-06-08 13:20:24 -0300

    Basic routes structure.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css';
+import Header from './components/Header'
+import Home from './rotas/Home'
+import NotFound from './rotas/Notfound'
+
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+<Header />
+ <Routes>
+   <Route path="/" element={<Home />} />
+   <Route path="*" element={<NotFound />} />
+ </Routes>
+ </BrowserRouter>
   );
 }
 
