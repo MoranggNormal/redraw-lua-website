ea9110fc1e4a13ed85022feda4027524a21ed480
eullerpeixoto <eullerz01@hotmail.com>
2021-06-08 15:40:26 -0300

    Loading content.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css';
 import Header from './components/Header'
 import Home from './rotas/Home'
 import NotFound from './rotas/Notfound'
+import Loading from './components/Loading'
+
 
 
 function App() {
+  const [loading, setLoading] = React.useState(false)
+
+  React.useEffect( () => {
+
+      setLoading(false)
+
+  }, [])
+
+
   return (
     <BrowserRouter>
 <Header />
