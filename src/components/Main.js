00fb2cc608d3b0e5e3ffd20374b5a0e1503be4fa
eullerpeixoto <eullerz01@hotmail.com>
2021-06-09 06:49:32 -0300

    Changed bg color.

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,21 +2,20 @@ import React from 'react'
 import styled from 'styled-components'
 
 const MAIN = styled.main`
-width: 75vw;
-height: 68vh;
-margin: 3rem auto;
-padding: 1rem;
-background: rgba( 5, 27, 90, 0.55 );
-
-box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
-backdrop-filter: blur( 3.0px );
--webkit-backdrop-filter: blur( 3.0px );
-border-radius: 10px;
-border: 1px solid rgba( 255, 255, 255, 0.18 );
-color: #232323;
-font-family: 'Open Sans', sans-serif;
-font-weight: 200;
-display: flex;
+  width: 75vw;
+  height: 68vh;
+  margin: 3rem auto;
+  padding: 1rem;
+  background: rgba( 216, 227, 231, 0.10 );
+  box-shadow: 0 8px 16px 0 rgba( 31, 38, 135, 0.37 );
+  backdrop-filter: blur( 4px );
+  -webkit-backdrop-filter: blur( 4px );
+  border-radius: 10px;
+  border: 1px solid rgba( 255, 255, 255, 0.18 );
+  color: #142850;
+  font-family: 'Open Sans', sans-serif;
+  font-weight: 200;
+  display: flex;
 `
 
 const Main = ({children}) => {
